72315fb360a04b292bcf43803c929d9e032ba9c0
Murphy Randle <m@mrmurphy.dev>
2020-01-21 10:52:25 -0700

    Update copyPlatformBinaryInPlace.js
    Don't add `.exe` onto the destination path for the executable.
    Hey Windows people, does this break you?

--- copyPlatformBinaryInPlace.js
+++ copyPlatformBinaryInPlace.js
@@ -8,7 +8,7 @@ var platform = process.platform === "win32" ? "win" : process.platform;
 
 var rootDir = __dirname;
 var sourceExePath = path.join(rootDir, `bs-let-${platform}-${arch}.exe`);
-var destExePath = path.join(rootDir, `ppx.exe`);
+var destExePath = path.join(rootDir, `ppx`);
 
 copyBinary(sourceExePath, destExePath);
 
